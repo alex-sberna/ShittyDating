fae0f96d05142aaeadccef12f1b57ba7c11388cc
Alex Sberna <alex.sberna@gmail.com>
2019-07-28 19:59:07 -0400

    added stuffs

--- ShittySpeedDating/ShittySpeedDating/MainWindow.xaml.cs
+++ ShittySpeedDating/ShittySpeedDating/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ShittySpeedDating.Types;
 
 namespace ShittySpeedDating
 {
@@ -23,6 +24,32 @@ namespace ShittySpeedDating
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void AddDateObject()
+        {
+            DateObject dateObject = new DateObject
+            {
+                Name = "Marvin",
+                Personality = "Outgoing",
+                Rating = 0,
+                QuestionObject1 = new QuestionObject
+                {
+                    Question = "What is your favorite color?",
+                    Answer1 = "Blue, like the ocean.",
+                    Answer1Score = 1,
+                    Answer2 = "Green, like grass.",
+                    Answer2Score = 3,
+                    Answer3 = "Yellow, like barf",
+                    Answer3Score = -2,
+                    Answer4 = "Red, like heart's blood.",
+                    Answer4Score = 5
+                },
+                QuestionObject2 = new QuestionObject
+                {
+
+                },
+            };
+            
         }
     }
 }
---
+++ ShittySpeedDating/ShittySpeedDating/Types/DateObject.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShittySpeedDating.Types
+{
+    public class DateObject
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Personality { get; set; }
+        public int Rating { get; set; }
+        public QuestionObject QuestionObject1 { get; set; }
+        public QuestionObject QuestionObject2 { get; set; }
+        public QuestionObject QuestionObject3 { get; set; }
+        public QuestionObject QuestionObject4 { get; set; }
+        public QuestionObject QuestionObject5 { get; set; }
+    }
+
+    public class QuestionObject
+    {
+        public string Question { get; set; }
+        public string Answer1 { get; set; }
+        public string Answer2 { get; set; }
+        public string Answer3 { get; set; }
+        public string Answer4 { get; set; }
+        public int Answer1Score { get; set; }
+        public int Answer2Score { get; set; }
+        public int Answer3Score { get; set; }
+        public int Answer4Score { get; set; }
+        public QuestionObject()
+        {
+            QuestionObject questionObject = new QuestionObject();
+        }
+    }
+}
